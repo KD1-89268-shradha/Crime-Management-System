790266ae6233f6d3863c125a13b1c52b6f0b4e98
UDAY DANOLE <Aachal>
2025-07-25 12:30:39 +0530

    police-home-page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './Pages/User-Pages/home'
 import {Routes, Route} from 'react-router-dom'
-import AdminHome from './Pages/Admin-Pages/admin_home'
+import Policehome from './Pages/Police-Pages/policehome'
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <div className='App'>
       <Routes>
         <Route path='/' element = {<Home/>}/>
-        <Route path ='/admin-home' element={<AdminHome/>}/>
+        <Route path='/policehome' element={<Policehome />}></Route>
       </Routes>
     </div>
   )
