19dadbc3ea6002e67153738e2a1ecdd087d15a6a
89283-Rutuja-Dhavale <dhavaler29@gmail.com>
2025-07-25 15:13:39 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from './Pages/User-Pages/home'
 import {Routes, Route} from 'react-router-dom'
 import Policehome from './Pages/Police-Pages/policehome'
+import AdminHome from './Pages/Admin-Pages/admin_home'
 
 
 
