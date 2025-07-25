c5d0a67aadc59530899ff36b03336b4baaae2712
89283-Rutuja-Dhavale <dhavaler29@gmail.com>
2025-07-25 14:51:18 +0530

    app changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,15 @@ import {Routes, Route} from 'react-router-dom'
 import Policehome from './Pages/Police-Pages/policehome'
 
 
+
+
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element = {<Home/>}/>
-<<<<<<< HEAD
-        <Route path ='/adminhome' element={<AdminHome/>}/>
         <Route path='/policehome' element={<Policehome />}></Route>
->>>>>>> 6fcdb2a1b76b84078d133224b069747742e75aab
+        <Route path ='/adminhome' element={<AdminHome/>}/>
       </Routes>
     </div>
   )
