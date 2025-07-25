0d2be4c134f4eaf5c42b62757722e160d878976f
AniketUgale <aniketUgale.scoe.it@gmail.com>
2025-07-25 15:50:49 +0530

    loginregisterUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Home from './Pages/User-Pages/home'
 import {Routes, Route} from 'react-router-dom'
 import Policehome from './Pages/Police-Pages/policehome'
-
+import AdminHome from './Pages/Admin-Pages/admin_home'
+import './App.css';
+import RegisterUser from './Pages/User-Pages/register'
+import UserLogin from './Pages/User-Pages/login'
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Routes>
         <Route path='/' element = {<Home/>}/>
         <Route path='/policehome' element={<Policehome />}></Route>
+        <Route path='/adminhome' element={<AdminHome />}></Route>
+        <Route path='/register' element={<RegisterUser />} />
+        <Route path='/user' element={<UserLogin />} />
+
       </Routes>
     </div>
   )
