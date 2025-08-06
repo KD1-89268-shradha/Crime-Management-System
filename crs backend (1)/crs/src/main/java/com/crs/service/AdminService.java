e71a698dfa3ec8a13a68168420ed29349a67eed6
AniketUgale932 <aniketugale198@gmail.com>
2025-08-06 08:25:17 +0530

    Delete crs backend (1)/crs/src/main/java/com/crs/service/AdminService.java

--- crs backend (1)/crs/src/main/java/com/crs/service/AdminService.java
+++
@@ -1,8 +0,0 @@
-package com.crs.service;
-
-import com.crs.dto.AdminDTO;
-
-public interface AdminService {
-    AdminDTO saveAdmin(AdminDTO adminDTO);
-    AdminDTO login(String email, String password);
-}
